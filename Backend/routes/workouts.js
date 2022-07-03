09aadefec64b91e86adf39152751359d48c0063f
junaidapdev <junaidap.dev.com>
2022-07-03 22:33:41 +0530

    workout post added

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const Workout = require("../models/workoutModel")
 
 const router = express.Router()
 
@@ -13,11 +13,19 @@ router.get('/:id', (req, res) => {
     res.json({msg: "Get a Single Workout"})
 })
 
-// POST a Single Workout
-router.post('/', (req, res) => {
-    res.json({msg: "Post a New Workout"})
-})
+// POST a new workout
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+    
+    try {
+      const workout = await Workout.create({title, load, reps})
+      res.status(200).json(workout)
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
+  })
 
+  
 // DELETE a Single Workout
 router.delete('/:id', (req, res) => {
     res.json({msg: "Delete a Single Workout"})
