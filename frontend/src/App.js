74aab570467401f8ec14592e8176c1f09e5c46cf
junaidapdev <junaidap.dev.com>
2022-07-04 13:28:01 +0530

    frontend: Navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 
 import {BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+
+      <Navbar/>
         <div className="pages">
           <Routes>
             <Route
