bb33b23fd70221dc2316cab734a4972eda583b24
junaidapdev <junaidap.dev.com>
2022-07-04 10:27:22 +0530

    get, add & update workout added

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,40 +1,20 @@
 const express = require("express")
-const Workout = require("../models/workoutModel")
+const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require("../controllers/workoutController")
 
 const router = express.Router()
 
 // GET all Workouts
-router.get('/', (req, res) => {
-    res.json({msg: "Get all Workouts"})
-})
+router.get('/', getWorkouts)
 
 // GET a Single Workout
-router.get('/:id', (req, res) => {
-    res.json({msg: "Get a Single Workout"})
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
-    
-    try {
-      const workout = await Workout.create({title, load, reps})
-      res.status(200).json(workout)
-    } catch (error) {
-      res.status(400).json({error: error.message})
-    }
-  })
+router.post('/', createWorkout)
 
-  
-// DELETE a Single Workout
-router.delete('/:id', (req, res) => {
-    res.json({msg: "Delete a Single Workout"})
-})
 
 // UPDATE a Workout
-router.patch('/:id', (req, res) => {
-    res.json({msg: "Update a Single Workout"})
-})
+router.patch('/:id', updateWorkout)
 
 
 module.exports = router
