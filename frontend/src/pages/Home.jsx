6ca936266d68344a9d9642951ab33898d1561b33
junaidapdev <junaidap.dev.com>
2022-07-04 13:42:53 +0530

    fetching data from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {useEffect, useState} from "react"
 
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("/api/workouts")
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+        fetchWorkouts()
+    }, [])
+
+
   return (
-    <div>Home</div>
-  )
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
